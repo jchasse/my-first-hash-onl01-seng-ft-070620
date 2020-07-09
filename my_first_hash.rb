4828dfc9feab09df3bb2f2da57fccf8da3f4db50
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-09 03:46:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  your_hash = {"name" => "Jon", "height" => 74}
   
 end
 
